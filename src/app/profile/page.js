0cdad644047f4daae355903ead194823ef6f49f6
JayIrv <135241031+JIrv30@users.noreply.github.com>
2023-12-17 22:15:25 +0000

    git ignore

--- src/app/profile/page.js
+++ src/app/profile/page.js
@@ -6,22 +6,35 @@ import { useEffect, useState } from "react"
 
 import toast from 'react-hot-toast';
 
-//to do - need to add error box - could refator success and info box to be more reusable
 
 export default function ProfilePage () {
   const session = useSession()
   const [userName, setUserName] = useState('')
   const [image, setImage] = useState('')
+  const [phone, setPhone] = useState('')
+  const [streetAddress, setStreetAddress] = useState('')
+  const [postalCode, setPostalCode] = useState('')
+  const [city, setCity] = useState('')
+  const [country, setCountry] = useState('')
   const {status} = session
 
+  
   useEffect(()=>{
     if(status==='authenticated') {
-      setUserName(session?.data?.user?.name)
+      setUserName(session.data.user.name)
       setImage(session.data.user.image)
+      fetch('/api/profile').then(response =>{
+        response.json().then(data=>{
+          setPhone(data.phone)
+          setStreetAddress(data.streetAddress)
+          setPostalCode(data.postalCode)
+          setCity(data.city)
+          setCountry(data.country)
+        })
+      })
     }
   },[session, status])
   
-  const userImage = session?.data?.user?.image
 
   async function handleProfileInfoUpdate (e) {
     e.preventDefault()
@@ -30,7 +43,15 @@ export default function ProfilePage () {
       const response = await fetch('/api/profile', {
         method: 'PUT',
         headers: {'Content-Type': 'application/json'},
-        body: JSON.stringify({name: userName,image})
+        body: JSON.stringify({
+          name: userName,
+          image,
+          streetAddress,
+          phone,
+          postalCode,
+          city,
+          country
+        })
       })
       if(response.ok) 
         resolve()
@@ -86,7 +107,7 @@ export default function ProfilePage () {
       </h1>
       
       <div className="max-w-md mx-auto ">
-        <div className="flex gap-4 items-center">
+        <div className="flex gap-4">
           <div>     
             <div className=" p-2 rounded-lg relative max-w-[120px]">
               {image && (<Image className="rounded-lg w-full h-full mb-1" src={image} width={250} height={250} alt="avatar" />)}
@@ -100,16 +121,61 @@ export default function ProfilePage () {
           </div>
 
           <form className="grow" onSubmit={handleProfileInfoUpdate}>
+            <label>First and last name</label>
             <input 
-            type="text" 
-            placeholder="First and last name"
-            value={userName}
-            onChange={e=>setUserName(e.target.value)} />
+              type="text" 
+              placeholder="First and last name"
+              value={userName}
+              onChange={e=>setUserName(e.target.value)} />
             
+            <label>email</label>
             <input 
-            type="email" 
-            value={session.data.user.email}
-            disabled={true} />
+              type="email"
+              placeholder="email" 
+              value={session.data.user.email || ''}
+              disabled={true} />
+            
+            <label>Phone</label>
+            <input
+             type="tel" 
+             placeholder="Phone Number"
+             value={phone}
+             onChange={e=>setPhone(e.target.value)}  />
+            
+            <label>Street Address</label>
+            <input 
+              type="text" 
+              placeholder="Street address"
+              value={streetAddress}
+             onChange={e=>setStreetAddress(e.target.value)}   />
+
+            <div className="flex gap-2">
+              <div>
+                <label>Postal Code</label>
+                <input
+                  type="text" 
+                  placeholder="Post code"
+                  value={postalCode}
+                  onChange={e=>setPostalCode(e.target.value)}   />  
+              </div>
+              
+              <div>
+                <label>city</label>
+                <input
+                  type="text"
+                  placeholder="city"
+                  value={city}
+                  onChange={e=>setCity(e.target.value)}   />
+              </div>
+              
+
+            </div>
+            <label>Country</label>
+            <input 
+              type="text" 
+              placeholder="Country"
+              value={country}
+              onChange={e=>setCountry(e.target.value)}   />
             
             <button type="submit">Save</button>
           </form>
